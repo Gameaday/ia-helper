c2aa888d56cd0dff6d57b23e52a35bb6b9d308d2
Gameaday <Gameaday@users.noreply.github.com>
2025-10-08 16:21:37 +0000

    deploy: c1a11db3c6e49e4b4cccef2241283e2492c5b8cd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4228250660"
+    serviceWorkerVersion: "2533426439"
   }
 });
